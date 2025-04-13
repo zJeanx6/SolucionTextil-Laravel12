941639d40bb6c4c76251475b337d23407e639742
Jean Roa <stebanrc21@gmail.com>
2025-04-13 09:03:49 -0500

    cleaning in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,36 +10,25 @@ use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $primaryKey = 'card';
+    protected $keyType = 'unsignedBigInteger';
+    public $incrementing = false;
+
     protected $fillable = [
+        'card',
         'name',
         'last_name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -48,9 +37,6 @@ class User extends Authenticatable
         ];
     }
 
-    /**
-     * Get the user's initials
-     */
     public function initials(): string
     {
         return Str::of($this->name)
