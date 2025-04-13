930520de64025c2e835439907b829ef00c7d072f
Jean Roa <stebanrc21@gmail.com>
2025-04-13 09:03:16 -0500

    Add spaces in controllers Brand, Role.

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -8,43 +8,58 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    public function index(){   
-        $brands = Brand::orderBy('id', 'desc')->paginate(10);
+
+    public function index()
+    {   
+        $brands = Brand::orderBy('id', 'desc')->paginate(12);
         return view('admin.brands.index', compact('brands'));
     }
 
-    public function create(){
+
+    public function create()
+    {
         return view('admin.brands.create');
     }
 
-    public function store(Request $request){
+
+    public function store(Request $request)
+    {
         $data = $request->validate([
             'name' => 'required|min:3|max:100'
         ]);
 
         Brand::create($data);
-        return redirect()->route('admin.brands.index')->with('success', 'Marca creada correctamente');
+        return redirect()->route('admin.brands.index')->with('success', 'Marca creada correctamente.');
     }
 
-    public function show(string $id){
+
+    public function show(string $id)
+    {
         //
     }
 
-    public function edit(Brand $brand){
+
+    public function edit(Brand $brand)
+    {
         return view('admin.brands.edit', compact('brand'));
     }
 
-    public function update(Request $request, Brand $brand){
+
+    public function update(Request $request, Brand $brand)
+    {
         $data = $request->validate([
             'name' => 'required|min:3|max:100'
         ]);
 
         $brand->update($data);
-        return redirect()->route('admin.brands.edit', $brand)->with('success', 'Marca actualizada correctamente');
+        return redirect()->route('admin.brands.edit', $brand)->with('success', 'Marca actualizada correctamente.');
     }
 
-    public function destroy(Brand $brand){
+
+    public function destroy(Brand $brand)
+    {
         $brand->delete();
-        return redirect()->route('admin.brands.index')->with('success', 'Marca eliminada correctamente');
+        return redirect()->route('admin.brands.index')->with('success', 'Marca eliminada correctamente.');
     }
+
 }
--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -8,16 +8,22 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    public function index(){
-        $roles = Role::orderBy('id', 'desc')->paginate(10);
+
+    public function index()
+    {
+        $roles = Role::orderBy('id', 'desc')->paginate(12);
         return view('admin.roles.index', compact('roles'));
     }
 
-    public function create(){
+
+    public function create()
+    {
         return view('admin.roles.create');
     }
 
-    public function store(Request $request){
+
+    public function store(Request $request)
+    {
         $data = $request->validate([
             'name' => 'required|string|min:3|max:255',
             'description' => 'required|string|min:10|max:255',
@@ -26,14 +32,21 @@ class RoleController extends Controller
         return redirect()->route('admin.roles.index')->with('success', 'Rol creado correctamente.');
     }
 
-    public function show(Role $role){
+    
+    public function show(Role $role)
+    {
+        //
     }
 
-    public function edit(Role $role){
+
+    public function edit(Role $role)
+    {
         return view('admin.roles.edit' , compact('role'));
     }
 
-    public function update(Request $request, Role $role){
+
+    public function update(Request $request, Role $role)
+    {
         $data = $request->validate([
             'name' => 'required|string|min:3|max:255',
             'description' => 'required|string|min:10|max:255',
@@ -42,8 +55,11 @@ class RoleController extends Controller
         return redirect()->route('admin.roles.edit', $role)->with('success', 'Rol actualizado correctamente.');
     }
 
-    public function destroy(Role $role){
+
+    public function destroy(Role $role)
+    {
         $role->delete();
         return redirect()->route('admin.roles.index')->with('success', 'Rol eliminado correctamente.');
     }
+
 }
