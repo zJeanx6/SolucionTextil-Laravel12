47e7ce0fdb25f3c3137bcf1bf47d31987cefe2df
Jean Roa <stebanrc21@gmail.com>
2025-05-20 17:11:30 -0500

    refactor: unify layouts (app/dashboard-public)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,53 +1,289 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
+@php
+    $groups = [
+        'Platform' => [
+            [
+                'name' => 'Dashboard',
+                'icon' => 'home',
+                'url' => route('dashboard'),
+                'current' => request()->routeIs('dashboard'),
+            ],
 
-    @livewireScripts
-    @stack('js')
+        ],
+        'Inventarios' =>[
+            [
+                'name' => 'Elementos',
+                'icon' => 'home',
+                'url' => route('admin.elements.index'),
+                'current' => request()->routeIs('admin.elements.index'),
+            ],
+            [
+                'name' => 'Productos',
+                'icon' => 'home',
+                'url' => route('admin.products.index'),
+                'current' => request()->routeIs('admin.products.index'),
+            ],
+            [
+                'name' => 'Maquinas',
+                'icon' => 'home',
+                'url' => route('admin.machines.index'),
+                'current' => request()->routeIs('admin.machines.index'),
+            ],           
+            [
+                'name' => 'Proveedores',
+                'icon' => 'users',
+                'url' => route('admin.suppliers.index'),
+                'current' => request()->routeIs('admin.suppliers.index'),
+            ],           
+        ],
+        'Complementos' => [
+            [
+                'name' => 'Tallas',
+                'icon' => 'swatch',
+                'url' => route('admin.sizes.index'),
+                'current' => request()->routeIs('admin.sizes.*'),
+            ],
+            [
+                'name' => 'Colores',
+                'icon' => 'swatch',
+                'url' => route('admin.colors.index'),
+                'current' => request()->routeIs('admin.colors.*'),
+            ],
+            [
+                'name' => 'Estados',
+                'icon' => 'cpu-chip',
+                'url' => route('admin.states.index'),
+                'current' => request()->routeIs('admin.states.*'),
+            ],
+            [
+                'name' => 'Roles',
+                'icon' => 'user-plus',
+                'url' => route('admin.roles.index'),
+                'current' => request()->routeIs('admin.roles.*'),
+            ],
+            [
+                'name' => 'Marcas',
+                'icon' => 'globe-americas',
+                'url' => route('admin.brands.index'),
+                'current' => request()->routeIs('admin.brands.*'),
+            ],
+            [
+                'name' => 'Tipos/Categorias',
+                'icon' => 'swatch',
+                'url' => route('admin.types.index'),
+                'current' => request()->routeIs('admin.types.index'),
+            ],
+            [
+                'name' => 'Tipos de Mantenimiento',
+                'icon' => 'swatch',
+                'url' => route('admin.maintenance.index'),
+                'current' => request()->routeIs('admin.maintenance.index'),
+            ],
+        ],
+    ];
+@endphp
 
-    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-    <script>
-        @if (session('success'))
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
-            var isDarkMode = document.documentElement.classList.contains('dark');
-            var toastBackgroundColor = isDarkMode ?
-                'linear-gradient(to right, #444444, #666666)' // modo oscuro
-                :
-                'linear-gradient(to right, #333333, #666666)'; // modo claro
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-            Toastify({
-                text: "{{ session('success') }}",
-                duration: 2000,
-                // close: true,
-                gravity: "top",
-                position: "center",
-                backgroundColor: toastBackgroundColor,
-                className: "toast-notification",
-                style: {
-                    borderRadius: "10px",
-                }
-            }).showToast();
-        @endif
-    </script>
+        <title>{{ $title ?? 'Laravel' }}</title>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        function confirmDelete(button) {
-            Swal.fire({
-                title: '¿Estás seguro?',
-                text: "¡No podrás revertir esta acción!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#27272a',
-                confirmButtonText: 'Sí, eliminar',
-                cancelButtonText: 'Cancelar'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    button.closest('form').submit();
-                }
-            });
-        }
-    </script>
-</x-layouts.app.sidebar>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxAppearance
+    </head>
+
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+
+            <a href="{{ route('dashboard') }}" class="mr-5 flex items-center space-x-2" wire:navigate>
+                <x-app-logo />
+            </a>
+
+            <flux:navlist variant="outline">
+                <flux:navlist.group :heading="'Platform'" class="grid">
+                    @foreach ($groups['Platform'] as $link)
+                        <flux:navlist.item :icon="$link['icon']" :href="$link['url']" :current="$link['current']"
+                            wire:navigate>{{ $link['name'] }}</flux:navlist.item>
+                    @endforeach
+                </flux:navlist.group>
+                <flux:navlist.group expandable :heading="'Complementos'" :expanded="collect($groups['Complementos'])->contains(fn($link)=>$link['current'])" class="grid">
+                    @foreach ($groups['Complementos'] as $link2)
+                        <flux:navlist.item :icon="$link2['icon']" :href="$link2['url']" :current="$link2['current']"
+                            wire:navigate>{{ $link2['name'] }}</flux:navlist.item>
+                    @endforeach
+                </flux:navlist.group>
+                <flux:navlist.group expandable :heading="'Inventarios'" :expanded="collect($groups['Inventarios'])->contains(fn($link)=>$link['current'])" class="grid">
+                    @foreach ($groups['Inventarios'] as $link2)
+                        <flux:navlist.item :icon="$link2['icon']" :href="$link2['url']" :current="$link2['current']"
+                            wire:navigate>{{ $link2['name'] }}</flux:navlist.item>
+                    @endforeach
+                </flux:navlist.group>
+            </flux:navlist>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit"
+                    target="_blank">
+                    {{ __('Repository') }}
+                </flux:navlist.item>
+
+                <flux:navlist.item icon="users" :href="route('admin.users.index')" :current="request()->routeIs('admin.users.*')">
+                    {{ __('Usuarios') }}
+                </flux:navlist.item>
+            </flux:navlist>
+
+            <!-- Desktop User Menu -->
+            <flux:dropdown position="bottom" align="start">
+                <flux:profile :name="auth()->user()->name" :initials="auth()->user()->initials()"
+                    icon-trailing="chevrons-up-down" />
+
+                <flux:menu class="w-[220px]">
+                    <flux:menu.radio.group>
+                        <div class="p-0 text-sm font-normal">
+                            <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                        {{ auth()->user()->initials() }}
+                                    </span>
+                                </span>
+
+                                <div class="grid flex-1 text-left text-sm leading-tight">
+                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('home')" icon="layout-grid" wire:navigate>{{ __('Home') }}</flux:menu.item>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <form method="POST" action="{{ route('logout') }}" class="w-full">
+                        @csrf
+                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                            {{ __('Log Out') }}
+                        </flux:menu.item>
+                    </form>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:sidebar>
+
+        <!-- Mobile User Menu -->
+        <flux:header class="lg:hidden">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+            <flux:spacer />
+
+            <flux:dropdown position="top" align="end">
+                <flux:profile :initials="auth()->user()->initials()" icon-trailing="chevron-down" />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <div class="p-0 text-sm font-normal">
+                            <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                        {{ auth()->user()->initials() }}
+                                    </span>
+                                </span>
+
+                                <div class="grid flex-1 text-left text-sm leading-tight">
+                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>
+                            {{ __('Settings') }}
+                        </flux:menu.item>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <form method="POST" action="{{ route('logout') }}" class="w-full">
+                        @csrf
+                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                            {{ __('Log Out') }}
+                        </flux:menu.item>
+                    </form>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:header>
+
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
+        @livewireScripts
+        @stack('js')
+
+        <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+        <script>
+            @if (session('success'))
+
+                var isDarkMode = document.documentElement.classList.contains('dark');
+                var toastBackgroundColor = isDarkMode ?
+                    'linear-gradient(to right, #444444, #666666)' // modo oscuro
+                    :
+                    'linear-gradient(to right, #333333, #666666)'; // modo claro
+
+                Toastify({
+                    text: "{{ session('success') }}",
+                    duration: 2000,
+                    // close: true,
+                    gravity: "top",
+                    position: "center",
+                    backgroundColor: toastBackgroundColor,
+                    className: "toast-notification",
+                    style: {
+                        borderRadius: "10px",
+                    }
+                }).showToast();
+            @endif
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            function confirmDelete(button) {
+                Swal.fire({
+                    title: '¿Estás seguro?',
+                    text: "¡No podrás revertir esta acción!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#27272a',
+                    confirmButtonText: 'Sí, eliminar',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        button.closest('form').submit();
+                    }
+                });
+            }
+        </script>
+    </body>
+</html>
--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -1,3 +1,152 @@
-<x-layouts.app.header :title="$title ?? null">
-    {{ $slot }}
-</x-layouts.app.header>
+@php
+    $links = [
+        [
+            'name' => 'Inicio',
+            'icon' => 'layout-grid',
+            'url' => route('home'),
+            'current' => request()->routeIs('home'),
+        ],
+    ];
+@endphp
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>{{ $title ?? 'Laravel' }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxAppearance
+    </head>
+
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+            <a href="{{ route('dashboard') }}" class="ml-2 mr-5 flex items-center space-x-2 lg:ml-0" wire:navigate>
+                <x-app-logo />
+            </a>
+
+            <flux:navbar class="-mb-px max-lg:hidden">
+                @foreach ($links as $link)
+                    <flux:navbar.item :icon="$link['icon']" :href="$link['url']" :current="$link['current']" wire:navigate>
+                        {{ $link['name']}}
+                    </flux:navbar.item>          
+                @endforeach
+            </flux:navbar>
+
+            <flux:spacer />
+
+            <!-- Desktop User Menu -->
+            @auth
+                <flux:dropdown position="top" align="end">
+                <flux:profile
+                    class="cursor-pointer"
+                    :initials="auth()->user()->initials()"
+                />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <div class="p-0 text-sm font-normal">
+                            <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
+                                    >
+                                        {{ auth()->user()->initials() }}
+                                    </span>
+                                </span>
+
+                                <div class="grid flex-1 text-left text-sm leading-tight">
+                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('dashboard')" icon="home" wire:navigate>{{ __('Dashboard') }}</flux:menu.item>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+                    <form method="POST" action="{{ route('logout') }}" class="w-full">
+                        @csrf
+                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                            {{ __('Log Out') }}
+                        </flux:menu.item>
+                    </form>
+
+                </flux:menu>
+                </flux:dropdown>
+            @else
+                <flux:dropdown position="top" align="end">
+                    <flux:button class="cursor-pointer" icon="user"/>
+                    <flux:menu>
+                        <flux:menu.radio.group>
+                            <flux:menu.item :href="route('login')" wire:navigate>
+                                {{ __('Log In') }}
+                            </flux:menu.item>
+                            <flux:menu.item :href="route('register')" wire:navigate>
+                                {{ __('Register') }}
+                            </flux:menu.item>
+                        </flux:menu.radio.group>
+                    </flux:menu>
+                </flux:dropdown>
+            @endauth
+
+        </flux:header>
+
+        <!-- Mobile Menu -->
+        <flux:sidebar stashable sticky class="lg:hidden border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+
+            <a href="{{ route('dashboard') }}" class="ml-1 flex items-center space-x-2" wire:navigate>
+                <x-app-logo />
+            </a>
+
+            <flux:navlist variant="outline">
+                <flux:navlist.group :heading="__('Platform')">
+                    @foreach ($links as $link)
+                        
+                        <flux:navlist.item :icon="$link['icon']" :href="$link['url']" :current="$link['current']" wire:navigate>
+                        {{ $link['name'] }}
+                        </flux:navlist.item>
+
+                    @endforeach
+                </flux:navlist.group>
+            </flux:navlist>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
+                {{ __('Repository') }}
+                </flux:navlist.item>
+
+                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits" target="_blank">
+                {{ __('Documentation') }}
+                </flux:navlist.item>
+            </flux:navlist>
+        </flux:sidebar>
+
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
+        @livewireScripts
+        @stack('js')
+    </body>
+</html>
