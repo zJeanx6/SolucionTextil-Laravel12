2c72a2665c4b27528c1067646b23279755596eb9
DaielaM1805 <daniela.manrique.mo@gmail.com>
2025-05-22 07:30:56 -0500

    fix: correccion barra de busqueda

--- resources/views/livewire/type-maintenance.blade.php
+++ resources/views/livewire/type-maintenance.blade.php
@@ -11,41 +11,46 @@
             <flux:button size="sm" variant="primary" wire:click="index">Volver</flux:button>
         @endif
     </div>
-    <div class="w-full mb-6">
-        <flux:input 
-            label="Barra de Búsqueda" type="text" wire:model.live="search" class="hover-input w-2/3 mx-auto px-4 py-2 border border-gray-300 rounded-md focus:outline-none  bg-white" name="search" placeholder="Buscar tipo de mantenimiento..." />
-    </div>
 
     @if ($view === 'index')
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="head-table">
-                    <tr>
-                        <th scope="col" class="px-6 py-2 text-center dark:text-white">NOMBRE</th>
-                        <th scope="col" class="px-6 py-2 text-center dark:text-white">DESCRIPCIÓN</th>
-                        <th scope="col" class="px-6 py-2 text-center dark:text-white">ACCIONES</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($maintenanceTypes as $type)
-                        <tr class="table-content">
-                            <th scope="row" class="px-6 py-4 text-center font-medium whitespace-nowrap">
-                                {{ $type->name }}</th>
-                            <td class="px-6 py-4 text-center uppercase">{{ $type->description }}</td>
-                            <td class="px-6 py-4 text-center">
-                                <div class="flex justify-center">
-                                    <flux:button size="sm" variant="primary"
-                                        wire:click="edit({{ $type->id }})">Editar</flux:button>
-                                    <flux:button size="sm" variant="danger"
-                                        wire:click="delete({{ $type->id }})">Eliminar</flux:button>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>      
-        <div class="mx-4 mt-4 mb-4">{{ $maintenanceTypes->links() }}</div>
+        <div class="card mb-4">
+            <div class="flex gap-4">
+                <div class="w-full">
+                    <flux:input type="text" wire:model.live="search" class="hover-input w-2/3 mx-auto px-4 py-2 " placeholder="Buscar tipo de mantenimiento..." />
+                </div>
+            </div>
+        </div>    
+
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="head-table">
+                            <tr>
+                                <th scope="col" class="px-6 py-2 text-center dark:text-white">NOMBRE</th>
+                                <th scope="col" class="px-6 py-2 text-center dark:text-white">DESCRIPCIÓN</th>
+                                <th scope="col" class="px-6 py-2 text-center dark:text-white">ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($maintenanceTypes as $type)
+                                <tr class="table-content">
+                                    <th scope="row" class="px-6 py-4 text-center font-medium whitespace-nowrap">
+                                        {{ $type->name }}</th>
+                                    <td class="px-6 py-4 text-center uppercase">{{ $type->description }}</td>
+                                    <td class="px-6 py-4 text-center">
+                                        <div class="flex justify-center">
+                                            <flux:button size="sm" variant="primary"
+                                                wire:click="edit({{ $type->id }})">Editar</flux:button>
+                                            <flux:button size="sm" variant="danger"
+                                                wire:click="delete({{ $type->id }})">Eliminar</flux:button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>      
+                <div class="mx-4 mt-4 mb-4">{{ $maintenanceTypes->links() }}</div>
+        </div>
     @elseif ($view === 'create')
         <div class="card">
             <form wire:submit.prevent="save">
