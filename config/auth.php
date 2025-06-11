f9abfb27c1acdb3d6539cf697aa55a1c896ef454
Jean Roa <stebanrc21@gmail.com>
2025-06-10 22:59:10 -0500

    feat: include laravel excel from maatwebsite

--- config/auth.php
+++ config/auth.php
@@ -60,6 +60,7 @@ return [
     */
 
     'providers' => [
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
