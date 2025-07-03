7e7f07add1ca6795d095fc28cdf80953f6d85efa
Jean Roa <stebanrc21@gmail.com>
2025-07-02 21:21:17 -0500

    refactor: remove obsolete dashboard components and views for element movements, pending loans, and weekly trends

--- resources/views/livewire/dashboard/pending-loans.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white dark:bg-neutral-800 p-6 flex flex-col items-center justify-center h-full">
-    <h3 class="text-base font-semibold text-black dark:text-white">Préstamos pendientes</h3>
-    <p class="text-6xl font-extrabold text-yellow-400 mt-2">{{ $pendientes }}</p>
-</div>
